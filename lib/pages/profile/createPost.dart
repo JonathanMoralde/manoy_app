58bf1b21ca225c7ed54ca9a45f31cdac62c66973
Jack <jackvincentsanchez.pobre@gmail.com>
2023-08-27 01:38:55 +0800

    modified UI for create post

--- lib/pages/profile/createPost.dart
+++ lib/pages/profile/createPost.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:manoy_app/widgets/styledButton.dart';
 
 class CreatePostPage extends StatefulWidget {
   const CreatePostPage({Key? key}) : super(key: key);
@@ -21,69 +22,105 @@ class _CreatePostPageState extends State<CreatePostPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('CREATE POST'),
+        backgroundColor: Colors.blue,
       ),
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextFormField(
-                controller: _postTextController,
-                maxLines: 6,
-                decoration: InputDecoration(
-                  hintText: "What's on your mind?",
-                  border: OutlineInputBorder(
-                    borderSide:
-                        BorderSide(color: Colors.blue), // Set border color here
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
+      body: Center(
+        child: Container(
+          width: 400,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Stack(
+                  alignment: Alignment.centerLeft,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                      ),
+                      child: Image.asset(
+                        'lib/images/logo.png', // Replace with your logo asset path
+                        width: 250,
+                        height: 250,
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Container(
-                height: 45,
-                width: 250,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Color(0xFF00A2FF),
-                  ),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.all(8),
-                    backgroundColor: Colors.white,
-                    foregroundColor: const Color.fromARGB(255, 97, 97, 97),
-                    elevation: 0,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextFormField(
+                    controller: _postTextController,
+                    maxLines: 8,
+                    decoration: InputDecoration(
+                      hintText: "What's on your mind?",
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: 16.0,
+                        horizontal: 16.0,
+                      ),
                     ),
                   ),
-                  onPressed: () {
-                    // Implement image upload logic here
-                  },
+                ),
+                Center(
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Expanded(
-                        child: Text(
-                          'Image',
-                          overflow: TextOverflow.ellipsis,
+                      Container(
+                        height: 45,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Color(0xFF00A2FF),
+                          ),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.all(8),
+                            backgroundColor: Colors.white,
+                            foregroundColor:
+                                const Color.fromARGB(255, 97, 97, 97),
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                          onPressed: () {
+                            // Implement image upload logic here
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  'Image',
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                              const Icon(
+                                Icons.upload,
+                                size: 24.0,
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                      const Icon(
-                        Icons.upload,
-                        size: 24.0,
-                      ),
+                      SizedBox(width: 30),
+                      StyledButton(btnText: 'POST', onClick: () {}),
                     ],
                   ),
                 ),
-              ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
